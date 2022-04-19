b7de41306d7c65389b89486008a642349b1ace66
Kaladar <ckaladar@gmail.com>
2022-04-19 19:27:42 +0530

    Downloaded body-parser. Used it to take string from the form and multiplied it by 10000 times and printed it. All that left is creating algorithm and adding it to the backend

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.listen(3000, function(){
   console.log("\nServer is active");
+})
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"//index.html");
+})
+
+app.post("/", function(req, res){
+  var str = String(req.body.str1) + " ";
+  str = str.repeat(100000);
+  res.send(str);
 })
