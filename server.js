b9421f595450218b05379d9133a7a088639d825b
Kaladar <ckaladar@gmail.com>
2022-04-21 06:37:55 +0530

    Wrote the function to return all permutations of given string

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const express = require("express");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+var original = "";
+var permutation = "";
+var all = [];
+
+
+function remove(str, i){
+  return str.substring(0, i) + str.substring(i+1, str.length);
+}
+
+function permute(original, permutation){
+  if(original === ""){
+    all.push(permutation);
+  }else{
+    var l = original.length;
+    for(let i = 0; i<l; i++){
+      var permutation2 = permutation + original.charAt(i);
+      var original2 = remove(original, i);
+      permute(original2,permutation2);
+    }
+  }
+}
+
 
 app.listen(3000, function(){
   console.log("\nServer is active");
@@ -13,7 +35,7 @@ app.get("/", function(req, res){
 })
 
 app.post("/", function(req, res){
-  var str = String(req.body.str1) + " ";
-  str = str.repeat(100000);
-  res.send(str);
+  original = String(req.body.str1);
+  permute(original, permutation);
+  res.send("All permutations of "+original+" = \n"+all.join(" "));
 })
